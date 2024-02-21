21cb79ce50b760483945b1a824f986bc50c33561
2018331109 <ahmadulsh@gmail.com>
2024-02-22 04:37:21 +0600

    Math

---
+++ Day-21_Math.cpp
@@ -0,0 +1,16 @@
+/*
+201. Bitwise AND of Numbers Range
+Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+*/
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int cnt=0;
+        while(left!=right)
+        {
+            left/=2, right/=2, cnt++;
+        }
+        return left<<cnt;
+        
+    }
+};
